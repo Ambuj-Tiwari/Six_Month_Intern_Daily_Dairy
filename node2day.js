6bc91b702c83d094ae70dfdf28b118c6d35c3a7b
Ambuj-Tiwari <ambujtiwari530@gmail.com>
2026-02-27 16:55:31 +0530

    Add examples for various Node.js core modules

--- node2day.js
+++ node2day.js
@@ -17,3 +17,59 @@ Understood how Node.js works internally and implemented core modules.
 
 ## Next Day Plan
 Learning Express.js framework.
+
+  id="blck1"
+const fs = require("fs");
+const data = fs.readFileSync("file.txt");
+console.log(data.toString());
+console.log("This runs after file read");
+
+# Non blocking 
+id="nblck1"
+const fs = require("fs");
+fs.readFile("file.txt", (err, data) => {
+  console.log(data.toString());
+});
+console.log("This runs immediately");
+
+# fs module 
+id="fs01"
+const fs = require("fs");
+
+fs.readFile("data.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log("Error reading file");
+    return;
+  }
+  console.log(data);
+});
+
+##http module 
+
+id="http01"
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.end("Day 2 Node.js Server Running");
+});
+
+server.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
+
+# path module 
+
+id="path01"
+const path = require("path");
+
+console.log(path.basename(__filename));
+console.log(path.extname(__filename));
+
+## Os module 
+
+id="os01"
+const os = require("os");
+
+console.log("OS:", os.platform());
+console.log("Free Memory:", os.freemem());
